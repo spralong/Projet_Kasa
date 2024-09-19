e41912b6b0f2f83a787418d39b168c995fe43e72
sebastien pralong <sebastien.pralong@free.fr>
2024-09-19 11:58:50 +0200

    Ajout des chevrons et css pour page About

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,1 +1,28 @@
 import React, { useState } from "react";
+
+const Collapse = ({ title, content }) => {
+  // Déclaration du state pour gérer l'ouverture/fermeture du collapse
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Fonction pour basculer l'état du collapse
+  const toggleCollapse = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="collapse_container">
+      <div className="collapse_title" onClick={toggleCollapse}>
+        <h2>{title}</h2>
+        <img
+          src={isOpen ? "./images/chevron-up.svg" : "./images/chevron-down.svg"}
+        />
+      </div>
+      {/* Affiche le contenu si le collapse est ouvert */}
+      {isOpen && (
+        <div className="collapse_content">
+          <p>{content}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Collapse;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import APropos from "../components/APropos";
+import Collapse from "../components/Collapse";
+import CollapseData from "../collapse.json";
+import Montagnes from "../images/Montagnes.png";
 
 const About = () => {
   return (
     <div>
       <Header />
-      <APropos />
+      <div className="container-about">
+        <img src={Montagnes} alt="Chaîne de montagnes"></img>
+      </div>
+      <div className="collapse">
+        {CollapseData.map((item) => {
+          return (
+            <div key={item.id}>
+              <Collapse content={item.content} title={item.title} />
+            </div>
+          );
+        })}
+      </div>
       <Footer />
     </div>
   );
